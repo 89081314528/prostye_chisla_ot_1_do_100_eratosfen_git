6b0da7ae207e593aa723fc9771c5e947c0981cb5
89081314528 <89081314528@mail.ru>
2019-09-02 21:17:23 +0300

    02/09 добавила печать в файл

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,46 +1,82 @@
 package com.company;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 public class Main {
 
-    public static void main(String[] args) {
-        int n = 50000;
-        int step = 10000;
-        long[] timeWork = new long[n];
-        for (int i = 1; i < n; i += step) {
+    public static void main(String[] args) throws FileNotFoundException {
+        printMassivProstyh(1000);
+        System.out.println();
+        long [] timeFound = timeFoundProstyhChisel(10000, 1000);
+        System.out.println("Массив с временем нахождения простых чисел");
+        for (int i = 0; i < timeFound.length; i ++) {
+            System.out.print(timeFound[i] + " ");
+        }
+
+        int n = 10_000;
+        int step = 1000;
+        PrintStream printer = new PrintStream(new File("timings.csv"));
+        printer.println("число" + ";" + "время");
+        for (int i = 0; i < timeFound.length; i ++) {
+            Time time = new Time(( i * step) + 2, timeFound[i]);
+            writeLine(time, printer);
+        }
+        printer.println();
+    }
+    public static void writeLine (Time time, PrintStream printer) {
+        printer.println(time.chislo + ";" + time.time);
+    }
+    public static long[] timeFoundProstyhChisel (int chislo, int step) {
+        int count = 0;
+        long[] massivTime = new long[chislo];
+        for (int i = 0; i < massivTime.length; i++) {
+            massivTime[i] = -1;
+        }
+        for (int i = 0; i < chislo; i += step) {
             long timeStart = System.currentTimeMillis();
             massivProstyh(i);
             long timeFinish = System.currentTimeMillis();
-            timeWork[i] = timeFinish - timeStart;
-            System.out.println("Время работы программы " + timeWork[i]);
+//            System.out.println("Время работы программы " + (timeFinish - timeStart));
+            massivTime[i] = timeFinish - timeStart;
+            count = count + 1;
         }
-        System.out.println("Массив с временем нахождения простых чисел от 1 до " + n + " с шагом " + step);
-        for (int i = 1; i < n; i += step) {
-            System.out.print(timeWork[i] + " ");
+        long[] newMassiveTime = new long[count];
+        int b = 0;
+        for (int i = 0; i < chislo; i++) {
+            if (massivTime[i] != -1) {
+                newMassiveTime[b] = massivTime[i];
+                b = b + 1;
+            }
         }
-
+        return newMassiveTime;
     }
-    public static void massivProstyh (int n) {
+
+        public static void printMassivProstyh(int chislo) {
+        int [] a = massivProstyh(chislo);
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a[i] + " ");
+        }
+    }
+
+    public static int[] massivProstyh (int n) {
         int massivProstyh[] = new int[n];
         for (int i = 2; i < n; i++) {
             massivProstyh[i] = i;
-//            System.out.print(i + " ");
         }
         for (int b = 2; b < n; b++) {
-            if (massivProstyh[b] > 0) {
                 for (int a = b * b; a < n; a += b) {
                     massivProstyh[a] = 0;
                 }
-            }
         }
         int count = 0;
         for (int b = 2; b < n; b++) {
             if (massivProstyh[b] > 0) {
                 count = count + 1;
             }
-//            System.out.print(massivProstyh[b] + " ");
         }
-//        System.out.println();
-        System.out.println("Количество простых чисел от 1 до " + n + " равно " + count);
+//        System.out.println("Количество простых чисел от 1 до " + n + " равно " + count);
         int[] massivProstyhChisel = new int[count];
         int b = 0;
         for (int i = 0; i < n; i++) {
@@ -49,9 +85,6 @@ public class Main {
                b = b + 1;
            }
         }
-        for (int i = 0; i < count; i ++) {
-            System.out.print(massivProstyhChisel[i] + " ");
-        }
-        System.out.println();
+        return massivProstyhChisel;
     }
 }
---
+++ src/com/company/Time.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Time {
+    long time;
+    int chislo;
+    public Time (int a, long b) {
+        time = b;
+        chislo = a;
+    }
+}
+
